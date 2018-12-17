fde6b994a9332f82f356700e9e634d9a32da8fe0
Giles Cope <gilescope@gmail.com>
2018-12-17 18:42:53 +0000

    Sketch but first working version.

--- src/main.rs
+++ src/main.rs
@@ -53,241 +53,84 @@ fn try_do() -> Result<(), Error> {
     }
     rl.save_history(".dockershell.history.txt").unwrap();
 
-
-
-
-    //List containers
-//    let filter = ContainerFilters::new();
-//    let containers = docker.list_containers(None, None, None, filter).unwrap();
-//
-//    containers.iter().for_each(|c| {
-//        docker.stop_container(&c.Id, Duration::from_secs(2)).unwrap();
-//        println!("ACTION: stopped {:?}", c);
-//        docker.remove_container(&c.Id, None, None, None).unwrap();
-//        println!("ACTION: removed {:?}", c);
-//    });
-
-//    //Create a continaer against the image
-//    let mut create = ContainerCreateOptions::new(&image);
-//    create.tty(true);
-//    create.entrypoint(vec!["/bin/sh".to_string(), "echo Hello world".to_string()]);
-//
-//    //instantiate container
-//    let container: CreateContainerResponse = docker.create_container(
-//        Some("my_container_name"), &create).unwrap();
-
-
-//    let filter = ContainerFilters::new();
-//    let containers = docker.list_containers(None, None, None, filter).unwrap();
-//    containers.iter().for_each(|c| {
-//        println!("QUERY: status {:?}", c.Status);
-//
-//        let opts = ContainerListOptions::default();
-//        let x = docker.filesystem_changes(c);
-//        let info = docker.container_info(c).unwrap();
-//        for mount in info.Mounts {
-//            println!("mount {:?}", mount);
-//        }
-//        if let Ok(y) = x {
-//            for change in y {
-//                println!("FOUND file sys changes::: {:#?}", change);
-//            }
-//        } else {
-//            println!("not found any {}", c)
-//        }
-
-//    let mut current_output: Option<String> = None;
-//    let mut current_command: Option<String> = None;
-//
-//    let histories = docker.history_image("myimage:latest");
-//    for history in histories {
-//        for event in history {
-//            //Gradually going back in time....
-//            println!("happened {:?} tags: {:?}", event.id, event.tags);
-//            // println!("happened {}", event.created_by);
-//
-//            if let Some(image) = event.id {
-//                //Remove any existing container with same name...
-//                let container_name = String::from("AA") + &(std::time::SystemTime::now().elapsed().unwrap()).as_secs().to_string();
-//                //println!("CONT name : {}", &container_name);
-//                docker.remove_container(&container_name, None, Some(true), None);
-//
-//                //Create container
-//                let mut create = ContainerCreateOptions::new(&image);
-//                let mut host_config = ContainerHostConfig::new();
-//                host_config.auto_remove(false);
-//                create.host_config(host_config);
-//                let it = command_line.iter();
-//                for command in it {
-//                    create.cmd(command.clone());
-//                }
-//
-//                let container: CreateContainerResponse = docker.create_container(
-//                    Some(&container_name), &create).unwrap();
-//
-//                docker.start_container(&container.id).unwrap();
-//
-//                let log_options = ContainerLogOptions {
-//                    stdout: true,
-//                    stderr: true,
-//                    since: None,
-//                    timestamps: None,
-//                    tail: None,
-//                };
-//
-//                std::thread::sleep(Duration::from_secs(2));
-//
-//                let mut container_output = String::new();
-//
-//                let result = docker.log_container_and_follow(&container_name, &log_options);
-//                if let Ok(result) = result {
-//                    let mut size = 1;
-//                    let mut line_reader = BufReader::new(result);
-//
-//                    while size != 0 {
-//                        size = line_reader.read_line(&mut container_output).unwrap();
-//                    }
-//                }
-//                //println!("{:?}", &container_output);
-//
-//                {
-//                    let expected = current_output.get_or_insert(container_output.clone());
-//
-//                    if expected != &container_output {
-//                        println!("{:?} changed to: {:?}", &container_output, &expected);
-//                        //Interesting...
-//                        println!("next command: {:?}", &current_command);
-//                        println!("previous command: {}", &event.created_by);
-//                    }
-//                }
-//                use std::mem; //when match it's a no-op
-//                mem::replace(&mut current_output, Some(container_output));
-//
-//                mem::replace(&mut current_command, Some(event.created_by));
-//
-//                docker.stop_container(&container.id, Duration::from_secs(2));
-//            }
-//        }
-//    }
-//
-//
-//    let f = File::open("/Users/gilescope/private/strat2/strategy-runner-py/Dockerfile")?;
-//    let mut reader = BufReader::new(f);
-//
-//    let mut lines = String::new();
-//
-//    reader.read_to_string(&mut lines)?;
-//
-//    let mut docker_file_so_far = String::new();
-//
-//    for line in lines.lines() {
-//        docker_file_so_far.push('\n');
-//        docker_file_so_far.push_str(line);
-////        println!("{}", docker_file_so_far);
-////        println!("-----------------------");
-//    }
     Ok(())
 }
 
-fn do_line(command_line: String) {
+/// Ok means the command was executed. Err means that docker couldn't find the command...
+fn do_line(command_lines: &Vec<String>) -> Result<(), ()>{
+    {
+        let mut dockerfile = File::create("Dockerfile").unwrap();
+        dockerfile.write_all(command_lines[..command_lines.len() - 1].join("\n").as_bytes()).unwrap();
+    }
+    // Create tar file
+    {
+        let tar_file = File::create("image.tar").unwrap();
+        let mut a = Builder::new(tar_file);
+        a.append_path("Dockerfile").unwrap();
+    }
+    let container_name: String = rand::thread_rng().gen_range(0., 1.3e4).to_string();
+    let image_name = String::from("img_") + &container_name;
+
+    let options = ContainerBuildOptions{
+        t: vec![image_name.to_owned()],
+        ..ContainerBuildOptions::default()
+    };
+
     let docker = Docker::connect_with_defaults().unwrap();
+    let res = docker.build_image(options, Path::new("image.tar")).unwrap();
+
+    for line in BufReader::new(res).lines() {
+        let buf = line.unwrap();
+        println!("{}", &buf);
+    }
+
     let mut host_config = ContainerHostConfig::new();
-    host_config.auto_remove(true);
-    let mut create = ContainerCreateOptions::new("alpine:edge");
+    host_config.auto_remove(true);//TODO?
+    let img_to_use = &(String::from(image_name) + ":latest");
+    println!("img to use: {}", img_to_use);
+    let mut create = ContainerCreateOptions::new(img_to_use);
     let mut args : Vec<String> = Vec::new();
 
-    args.push("/bin/sh".into());
-    args.push("-c".into());
-
+    let command_line = &command_lines[command_lines.len() - 1];
     for arg in command_line.split(' ') {
         args.push(String::from(arg));
     }
-    create.entrypoint(args);
+    args.remove(0); //asserrt [0] == RUN
+    println!("running cmd: {:?}", &args);
+//    create.entrypoint(args);
+    for ar in args {
+        create.cmd(ar);
+    }
 
-//    create.entrypoint(vec!["/bin/echo".into(),"hi".into()]);
     create.host_config(host_config);
-    let container_name: String = rand::thread_rng().gen_range(0., 1.3e4).to_string();
 
     let container = docker.create_container(Some(&container_name), &create).unwrap();
-    docker.start_container(&container.id).unwrap();
     let res = docker
         .attach_container(&container.id, None, true, true, false, true, false)
         .unwrap();
-    let cont: AttachContainer = res.into();
-    let mut line_reader = BufReader::new(cont.stdout);
+    let result = docker.start_container(&container.id);
 
-    loop {
-        let mut line = String::new();
-        let size = line_reader.read_line(&mut line).unwrap();
-        print!("{:4}: {}", size, line);
-        if size == 0 {
-            break;
-        }
+    let mut results = Vec::<String>::new();
+    match result {
+        Ok(_) => {
+            println!("starting container id  {} with name  {} ",container.id, &container_name);
+            let cont: AttachContainer = res.into();
+            let mut line_reader = BufReader::new(cont.stdout);
+
+            loop {
+                let mut line = String::new();
+                let size = line_reader.read_line(&mut line).unwrap();
+                print!("{}", line);
+                if size == 0 {
+                    break;
+                }
+                results.push(line);
+            }
+        },
+        Err(err) => {println!("{:?}", err); return Err(())}
     }
-    println!("");
-
-//    docker.
-
-//
-//    let docker = Docker::connect_with_defaults().unwrap();
-//    let image = "alpine:edge";// "hello-world:linux";
-//    let mut create = ContainerCreateOptions::new(&image);
-//    create.tty(true);
-//    let mut args : Vec<String> = Vec::new();
-//
-//    for arg in command_line.split(' ') {
-//        args.push(String::from(arg));
-//    }
-//    create.entrypoint(args);
-//    let container_name: String = rand::thread_rng().gen_range(0., 1.3e4).to_string();
-//
-//    //instantiate container
-//    let container: CreateContainerResponse = docker.create_container(
-//        Some(&container_name), &create).unwrap();
-//
-//    let res = docker
-//        .attach_container(&container.id, None, true, true, true, true, true)
-//        .unwrap();
-//    let cont: AttachContainer = res.into();
-//    let mut line_reader = BufReader::new(cont.stdout);
-//    let mut line_reader_err = BufReader::new(cont.stderr);
-//
-//    println!("Container ID: {}", &container.id);
-//    docker.start_container(&container.id).unwrap();
-//
-//
-//    loop {
-//        let mut line = String::new();
-//        let res = line_reader.read_line(&mut line);
-//        match res {
-//            Ok(size) => {
-//                if size > 0 {
-//                    print!("OUT: {:4}: {}", size, line);
-//                }
-////                if size == 0 {
-////                    break;
-////                }
-//            },
-//            Err(e) => {
-//                println!("OUT: {}", e);
-//            }
-//        }
-//
-//        let res = line_reader_err.read_line(&mut line);
-//        match res {
-//            Ok(size) => {
-//                if size > 0 {
-//                    print!("ERR: {:4}: {}", size, line);
-//                }
-////                if size == 0 {
-////                    break;
-////                }
-//            },
-//            Err(e) => {
-//                println!("ERR: {}", e);
-//            }
-//        }
-//    }
+    println!();
+    Ok(())
 }
 
 
